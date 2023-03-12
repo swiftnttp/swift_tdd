6551896010f0e95bc82893f53d01dd6489cccfd7
Alexey <alexeylevchenko@icloud.com>
2023-03-12 18:59:33 +0200

    1

--- fizzbuzz/Tests/fizzbuzzTests/fizzbuzzTests.swift
+++ fizzbuzz/Tests/fizzbuzzTests/fizzbuzzTests.swift
@@ -2,10 +2,7 @@ import XCTest
 @testable import fizzbuzz
 
 final class fizzbuzzTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(fizzbuzz().text, "Hello, World!")
+    func testOne() throws {
+        XCTAssertEqual(fizzbuzz(1), "1")
     }
 }
