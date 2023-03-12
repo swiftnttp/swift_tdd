5b6378cbce8555738a14a943e692cd02e70a0c7f
Alexey <alexeylevchenko@icloud.com>
2023-03-12 20:15:52 +0200

    init

--- fibonacci/Tests/fibonacciTests/fibonacciTests.swift
+++ fibonacci/Tests/fibonacciTests/fibonacciTests.swift
@@ -1,11 +1,11 @@
 import XCTest
 @testable import fibonacci
 
+//0, 1, 2, 3, 4, 5, 6, 7,  8,  9
+//0, 1, 1, 2, 3, 5, 8, 13, 21, 34
+
 final class fibonacciTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(fibonacci().text, "Hello, World!")
+    func testFibonacci() throws {
+        XCTAssertEqual(fibonacci(0), 0)
     }
 }
