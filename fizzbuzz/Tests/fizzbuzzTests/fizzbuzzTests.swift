ed905cb7553e2927bd7e6cf9a3fda7f4e7bfdda8
ig0ra <igor.obrizko@ukr.net>
2023-03-12 19:08:16 +0200

    1

--- fizzbuzz/Tests/fizzbuzzTests/fizzbuzzTests.swift
+++ fizzbuzz/Tests/fizzbuzzTests/fizzbuzzTests.swift
@@ -5,4 +5,10 @@ final class fizzbuzzTests: XCTestCase {
     func testOne() throws {
         XCTAssertEqual(fizzbuzz(1), "1")
     }
+    
+    func fizzbuzz(_ int: Int) -> String {
+        return "1"
+    }
+    
+    
 }
