74ea7bed7166a1ff87c453ff1ae2e4b558b54c83
ig0ra <igor.obrizko@ukr.net>
2023-03-15 18:27:39 +0200

    4

--- RomanNumerals/Tests/RomanNumeralsTests/RomanNumeralsTests.swift
+++ RomanNumerals/Tests/RomanNumeralsTests/RomanNumeralsTests.swift
@@ -6,16 +6,14 @@ final class RomanNumeralsTests: XCTestCase {
         XCTAssertEqual(romanNumerals(1), "I")
         XCTAssertEqual(romanNumerals(2), "II")
         XCTAssertEqual(romanNumerals(3), "III")
+        XCTAssertEqual(romanNumerals(4), "IV")
     }
     
     func romanNumerals(_ num: Int) -> String {
-        if num == 3 {
-            return "III"
+        if num <= 3, num > 1 {
+            return "I" + romanNumerals(num - 1)
         }
-        
-        if num == 2 {
-            return "II"
-        }
+
         return "I"
     }
 }
