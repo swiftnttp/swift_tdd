087942e222afa3220470f934b2e415e0e430601b
Alexey <alexeylevchenko@icloud.com>
2023-03-15 19:45:02 +0200

    true

--- BooleanCalculator/Tests/BooleanCalculatorTests/BooleanCalculatorTests.swift
+++ BooleanCalculator/Tests/BooleanCalculatorTests/BooleanCalculatorTests.swift
@@ -1,11 +1,20 @@
 import XCTest
 @testable import BooleanCalculator
 
+//Supports single values:
+//  "TRUE" -> true
+//  "FALSE" -> false
+//Supports the NOT operator:
+//   "NOT TRUE" -> false
+//Supports the AND operator:
+//   "TRUE AND FALSE" -> false
+//   "TRUE AND TRUE" -> true
+//Supports the OR operator:
+//   "TRUE OR FALSE" -> true
+//    "FALSE OR FALSE" -> false
+
 final class BooleanCalculatorTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(BooleanCalculator().text, "Hello, World!")
+    func testExample() {
+        XCTAssertEqual(calculate("TRUE"), true)
     }
 }
