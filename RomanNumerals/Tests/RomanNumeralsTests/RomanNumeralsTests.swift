6366fd97afe979ee97b2b18437a8c06b3b39be40
ig0ra <igor.obrizko@ukr.net>
2023-03-15 18:13:29 +0200

    ii

--- RomanNumerals/Tests/RomanNumeralsTests/RomanNumeralsTests.swift
+++ RomanNumerals/Tests/RomanNumeralsTests/RomanNumeralsTests.swift
@@ -3,6 +3,11 @@ import XCTest
 
 final class RomanNumeralsTests: XCTestCase {
     func testNumerals() throws {
-       XCTAssertEqual(romanNumerals(1), "I")
+        XCTAssertEqual(romanNumerals(1), "I")
+        XCTAssertEqual(romanNumerals(2), "II")
+    }
+    
+    func romanNumerals(_ num: Int) -> String {
+        "I"
     }
 }
