31ee95289253668c04377702278612cce3c6e2cb
Alexey <alexeylevchenko@icloud.com>
2023-03-27 18:46:23 +0300

    1

--- TicTacToe/Tests/TicTacToeTests/TicTacToeTests.swift
+++ TicTacToe/Tests/TicTacToeTests/TicTacToeTests.swift
@@ -1,11 +1,28 @@
 import XCTest
 @testable import TicTacToe
 
+//Tic-Tac-Toe rules:
+//• X always goes first
+//• Players alternate placing X's and O's on the board
+//• Players cannot play on a played position
+//• A player with three X's or O's in a row (horizontally, vertically, or diagonally) wins
+//• If all nine squares are filled and neither player achieves three in a row, the game is a draw
+
+struct Board {
+    
+    var moves: [String] = []
+    
+    func applyMove(_ player: String) {
+        
+    }
+}
+
 final class TicTacToeTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(TicTacToe().text, "Hello, World!")
+    func testPlayerXGoesFirst() throws {
+        let board = Board()
+        
+        board.applyMove("X")
+        
+        XCTAssertEqual(board.moves, ["X"])
     }
 }
