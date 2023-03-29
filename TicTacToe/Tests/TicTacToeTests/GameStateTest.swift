3d238de4ec4b886cebb01f5343b17b05994d94f0
Alexey <alexeylevchenko@icloud.com>
2023-03-29 19:15:19 +0300

    user x won

--- TicTacToe/Tests/TicTacToeTests/GameStateTest.swift
+++ TicTacToe/Tests/TicTacToeTests/GameStateTest.swift
@@ -7,8 +7,13 @@
 
 import XCTest
 
-enum GameState {
+enum Player {
+    case X, Y
+}
+
+enum GameState: Equatable {
     case gameInProgress
+    case userWon(Player)
 }
 
 final class GameStateTest: XCTestCase {
@@ -21,7 +26,27 @@ final class GameStateTest: XCTestCase {
         XCTAssertEqual(state, .gameInProgress)
     }
     
+    func testXWonVertically() {
+        let board = Board()
+            .applyMoveUnsafe(Position(x: 0, y: 0))
+            .applyMoveUnsafe(Position(x: 2, y: 0))
+            .applyMoveUnsafe(Position(x: 0, y: 1))
+            .applyMoveUnsafe(Position(x: 2, y: 1))
+            .applyMoveUnsafe(Position(x: 0, y: 2))
+        
+        
+        let state = gameState(board)
+        
+        XCTAssertEqual(state, .userWon(.X))
+    }
+    
     func gameState(_ board: Board) -> GameState {
         return .gameInProgress
+    }
+}
+
+private extension Board {
+    func applyMoveUnsafe(_ position: Position) -> Board {
+        try! applyMove(position).get()
     }
 }
