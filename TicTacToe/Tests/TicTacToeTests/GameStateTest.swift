c059cee8898bf98b2b36c7e4d67fc31e89764d22
ig0ra <igor.obrizko@ukr.net>
2023-03-29 19:03:26 +0300

    state

--- TicTacToe/Tests/TicTacToeTests/GameStateTest.swift
+++ TicTacToe/Tests/TicTacToeTests/GameStateTest.swift
@@ -7,9 +7,21 @@
 
 import XCTest
 
+enum GameState {
+    case gameInProgress
+}
+
 final class GameStateTest: XCTestCase {
 
     func testFoo() {
+        let board = Board()
         
+        let state = gameState(board)
+        
+        XCTAssertEqual(state, .gameInProgress)
+    }
+    
+    func gameState(_ board: Board) -> GameState {
+        return .gameInProgress
     }
 }
