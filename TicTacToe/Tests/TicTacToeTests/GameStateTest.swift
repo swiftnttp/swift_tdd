3ab11da9b4b335eea14568d89b5b402b4a80f9e9
Alexey <alexeylevchenko@icloud.com>
2023-03-31 18:22:21 +0300

    diagonal

--- TicTacToe/Tests/TicTacToeTests/GameStateTest.swift
+++ TicTacToe/Tests/TicTacToeTests/GameStateTest.swift
@@ -54,6 +54,20 @@ final class GameStateTest: XCTestCase {
         XCTAssertEqual(state, .userWon(.X))
     }
     
+    func testXWonDiagonally() {
+        let board = Board()
+            .applyMoveUnsafe(Position(x: 0, y: 0))
+            .applyMoveUnsafe(Position(x: 0, y: 2))
+            .applyMoveUnsafe(Position(x: 1, y: 1))
+            .applyMoveUnsafe(Position(x: 0, y: 1))
+            .applyMoveUnsafe(Position(x: 2, y: 2))
+        
+        
+        let state = gameState(board)
+        
+        XCTAssertEqual(state, .userWon(.X))
+    }
+    
     func gameState(_ board: Board) -> GameState {
         for i in 0...2 {
             let column = board.moves.filter { $0.position.x == i }
@@ -73,6 +87,13 @@ final class GameStateTest: XCTestCase {
                     return .userWon(row[0].player == "X" ? .X : .Y)
                 }
             }
+        }
+        
+        if board.moves.contains(where: { $0.position.y == 0 && $0.position.x == 0 }) &&
+            board.moves.contains(where: { $0.position.y == 1 && $0.position.x == 1 }) &&
+            board.moves.contains(where: { $0.position.y == 2 && $0.position.x == 2 }) {
+            let player = board.moves.first(where: { $0.position.y == 0 && $0.position.x == 0 })!.player
+            return .userWon(player == "X" ? .X : .Y)
         }
 
         return .gameInProgress
